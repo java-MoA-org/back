70e6a757cbf72c8404ccede942172986df797c8c
yuki-k1313 <yuki1313@naver.com>
2025-04-23 17:44:20 +0900

    페이지네이션 수정

--- src/main/java/com/MoA/moa_back/common/enums/BoardTagType.java
+++ src/main/java/com/MoA/moa_back/common/enums/BoardTagType.java
@@ -2,23 +2,12 @@ package com.MoA.moa_back.common.enums;
 
 // 게시글 태그 타입 //
 public enum BoardTagType {
-  GAME("게임"),
-  TRAVEL("여행"),
-  WORKOUT("운동"),
-  MUSIC("음악"),
-  ECONOMY("경제"),
-  FASHION("패션"),
-  FOOD("음식"),
-  FREE("자유");
-
-  private final String displayName;
-
-  BoardTagType(String displayName) {
-    this.displayName = displayName;
-  }
-
-  public String getDisplayName() {
-    return displayName;
-  }
-
+  GAME,
+  TRAVEL,
+  WORKOUT,
+  MUSIC,
+  ECONOMY,
+  FASHION,
+  FOOD,
+  FREE;
 }
